cd358ce5c8c2d48a2a352268594f2a1a0d083058
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-11-01 16:08:06 -0400

    did oops 1 videos

---
+++ src/com/raunaksinghinventor/hello_world/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.raunaksinghinventor.hello_world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		System.out.println(myFirstNumber);
+
+		int mySecondNumber = 10;
+		System.out.println(mySecondNumber);
+
+		Boolean myBoolean = null;
+		System.out.println(myBoolean);
+	}
+}
---
+++ src/com/raunaksinghinventor/oops1/classes/Car.java
@@ -0,0 +1,10 @@
+package com.raunaksinghinventor.oops1.classes;
+
+public class Car {
+
+	private int doors;
+	private int wheels;
+	private String model;
+	private String engine;
+	private String colour;
+}
