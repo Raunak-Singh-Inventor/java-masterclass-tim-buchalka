9513f9d041e64bd2a14e5bdbfb775ffd173e8026
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-11-07 11:12:35 -0500

    did reverse array challenge

--- src/com/raunaksinghinventor/arrays/Main.java
+++ src/com/raunaksinghinventor/arrays/Main.java
@@ -1,4 +1,34 @@
 package com.raunaksinghinventor.arrays;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myInts = getInts(5);
+        for(int i = 0; i < myInts.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myInts[i]);
+        }
+        System.out.println("The average is " + getAvg(myInts));
+    }
+
+    public static int[] getInts(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+        for(int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAvg(int[] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum+=array[i];
+        }
+        return (double) sum/array.length;
+    }
 }
---
+++ src/com/raunaksinghinventor/reverse_array/Main.java
@@ -0,0 +1,23 @@
+package com.raunaksinghinventor.reverse_array;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array)); // [1,2,3,4,5,6]
+        int mid = array.length/2; // mid = index 3
+        int j = array.length-1; // last index
+        for(int i = 0; i < mid; i++) {
+            // switch indexes i and j
+            int tmp = array[j];
+            array[j] = array[i];
+            array[i] = tmp;
+            j--;
+        }
+        System.out.println("Reversed Array = " + Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        reverse(new int[] {0,1,2,3,4});
+    }
+}
