fdf0f2e00d773500dfad75253c4c364b26631dfe
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-11-14 11:44:44 -0500

    did interface part 1

--- src/com/raunaksinghinventor/interfaces/Main.java
+++ src/com/raunaksinghinventor/interfaces/Main.java
@@ -3,6 +3,9 @@ package com.raunaksinghinventor.interfaces;
 public class Main {
 
     public static void main(String[] args) {
-        
+        ITelephone raunaksPhone = new DeskPhone(123456);
+        raunaksPhone.powerOn();
+        raunaksPhone.callPhone(123456);
+        raunaksPhone.answer();
     }
 }
